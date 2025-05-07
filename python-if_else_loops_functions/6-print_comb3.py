61ed0d61ef9db77da98da22e605283fed4fdeef6
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-05-07 20:14:41 +0000

    edit file 6 print comb3

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for n in range(10):
     for i in range(10):
-        if(n is 8 and i is 9):
+        if (n is 8 and i is 9):
             print("{}".format(str(n) + str(i)))
-        elif(i > n):
+        elif (i > n):
             print("{}".format(str(n) + str(i)) + ", ", end="")
