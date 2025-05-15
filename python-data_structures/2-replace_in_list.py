8ca1985faabc62d67195672957a135af006e3d33
Your Name <you@example.com>
2025-05-15 18:01:06 +0000

    edit file replace in list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,6 +3,6 @@ def replace_in_list(my_list, idx, element):
     if (idx < 0 or idx > len(my_list)-1):
         return my_list
     else:
-        for i in len(my_list):
+        for i in my_list:
             if (i == idx ):
                 my_list[i] = element
