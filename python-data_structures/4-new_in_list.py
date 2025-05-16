149d6a79cbe22b04e3f82c1f2201b6b6274c7920
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-05-16 13:06:51 +0000

    edit the list in file 4 new in list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     list = my_list
     if (idx < 0 or idx > len(my_list)-1):
-        return my_list
+        return list
     else:
         for i in range(len(my_list)):
             if (i == idx):
