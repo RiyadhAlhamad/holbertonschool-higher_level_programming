2b10213b11d35e49959d047f43a79642bafad134
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-05-16 23:48:47 +0000

    edit file 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,12 +2,12 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-        
+
     max_key = None
     max_value = float('-inf')
     for i in a_dictionary:
         if a_dictionary[i] > max_value:
             max_value = a_dictionary[i]
             max_key = i
-    
+
     return max_key
