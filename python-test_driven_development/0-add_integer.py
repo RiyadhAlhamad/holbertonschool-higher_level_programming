97f10b87b8e3bf221e8bed5b5c19635f510eeb50
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-05-17 00:53:40 +0000

    edit file 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    count = 0
+"""
 
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end='')
-        except IndexError:
-            break
-        else:
-            count += 1
+This module is composed by a function that adds two numbers
 
-    print()
-    return (count)
+"""
+
+
+def add_integer(a, b=98):
+    """ Function that adds two integer and/or float numbers
+
+    Args:
+        a: first number
+        b: second number
+
+    Returns:
+        The addition of the two given numbers
+
+    Raises:
+        TypeError: If a or b aren't integer and/or float numbers
+
+    """
+
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return (a + b)
