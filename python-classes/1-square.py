8eb74dd08b42a74fce543707891789a36709ab67
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-05-21 00:46:06 +0000

    edit file 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 class Square:
-    """Class that defines a square by its size."""
-
-
+    """
+    Class that defines a square by its size.
+    """
     def __init__(self, size):
-        """Initialize the square with a private size attribute."""
         self.__size = size
+        """Initialize the square with a private size attribute."""
+        
