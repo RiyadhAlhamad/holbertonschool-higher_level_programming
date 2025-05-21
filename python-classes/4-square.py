1e74d1b533364cccfcdf61c5d813a8b4f6aa8335
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-05-21 04:31:52 +0000

    add file 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -25,3 +25,18 @@ class Square:
     def area(self):
         """Returns the area of Square."""
         return self.__size * self.__size
+    
+    @property
+    def size(self):
+        """Return: size"""
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """Setter for set size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
