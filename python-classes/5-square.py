dbfe3b4d9c1dffc752b4b04b2d72587cbf99e4da
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-05-21 04:35:27 +0000

    delet folder pycache

---
+++ python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Module that defines a class Square"""
+
+
+class Square:
+    """Represents the square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+
+        Args:
+            size (int): the size of the square.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Returns the area of Square."""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Return: size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for set size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
