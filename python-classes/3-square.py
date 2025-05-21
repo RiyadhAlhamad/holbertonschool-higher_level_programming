bcc14076debc3dc919c971b747c4dc20e01b162d
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-05-21 04:22:46 +0000

    edit file 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -21,6 +21,7 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = size
-    
+
     def area(self):
-        return size * size
+        """Returns the area of Square."""
+        return self.__size * self.__size
