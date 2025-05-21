5f822a2ba1a2bb7791b8549cd1b75ec167feb73f
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-05-21 01:09:03 +0000

    edit file 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 class Square:
     """Class that defines a square by its size."""
-    
+        
     def __init__(self, size):
         """Initialize the square with a size"""
         self.__size = size
