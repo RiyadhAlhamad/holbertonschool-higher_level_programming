f2aeb8361a82af7a48c830a5f0dd1e2e0e64a8d8
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-05-21 11:37:11 +0000

    add file 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,66 +1,49 @@
 #!/usr/bin/python3
-"""Module that defines a class Square"""
+""" Module Square """
 
 
 class Square:
-    """Represents the square."""
-
+    """ Define a square by size """
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize a new Square.
-
-
-        Args:
-            size (int): the size of the square.
-            position tuple: the position the square.
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
-        """
-        self.__position = position
         self.__size = size
+        self.position = position
 
     def area(self):
-        """Returns the area of Square."""
-        return self.__size * self.__size
-
-    def my_print(self):
-        """print the Square"""
-        if self.__size == 0:
-            print()
-        else:
-            print("\n" * self.__position[1], end="")
-            for i in range(self.size):
-                print("_" * self.__position[0], end="")
-                print("#" * self.size)
+        return self.__size ** 2
 
     @property
     def size(self):
-        """Return: size"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Setter for set size"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
 
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            print("\n" * self.__position[1], end="")
+            for i in range(self.size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.size)
+
     @property
     def position(self):
-        """Return: position"""
-        return self.position
+        return self.__position
 
     @position.setter
     def position(self, value):
-        """Setter for set position"""
-        if value[0] < 0 and value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif not isinstance(value, tuple):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif not isinstance(value[0], int) and not isinstance(valut[1], int):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        msg = 'position must be a tuple of 2 positive integers'
+        if len(value) != 2 or type(value) != tuple:
+            raise TypeError(msg)
+        if type(value[0]) != int or value[0] < 0:
+            raise TypeError(msg)
+        if type(value[1]) != int or value[1] < 0:
+            raise TypeError(msg)
+        self.__position = value
