7cd42d96b8a4fdde25c065ab1ba1b5649cbc13dd
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-05-21 00:34:44 +0000

    add some file for task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """Class that dfines a square by its size"""
+
+
+    def __init__(self, size):
+        """
+        Initialize a new Square instance
+        
+        Args:
+            size: size of the square
+        """
+        self.__size = size
