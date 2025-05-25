2d6873f32ee149a90a21f483104a312f211e8b22
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-05-25 08:54:04 +0000

    edit file 100

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -36,13 +36,34 @@ class Node:
         """Return: next node."""
         return self.next_node
 
+
 class SinglyLinkedList:
     """Represents a SinglyLinkedList."""
 
     def __init__(self):
-        self.__head = self
-        print(self.__head)
+        self.__head = None
 
     def sorted_insert(self, value):
         """Insert Node."""
-        self.__head = value
+        new_node = Node(value)
+
+        if self.__head is None:
+            new_node.next_node = None
+            self.__head = new_node
+        elif self.__head.data > value:
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            temp = self.__head
+            while temp.next_node is not None and temp.next_node.data < value:
+                temp = temp.next_node
+            new_node.next_node = temp.next_node
+            temp.next_node = new_node
+
+    def __str__(self):
+        values = []
+        tmp = self.__head
+        while tmp is not None:
+            values.append(str(tmp.data))
+            tmp = tmp.next_node
+        return ('\n'.join(values))
