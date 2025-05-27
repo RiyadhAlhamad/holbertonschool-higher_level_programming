44dde7b20d080cabb6a4d423cb7b759ed32b1a76
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-05-27 08:51:26 +0000

    edit file 102

--- python-classes/102-square.py
+++ python-classes/102-square.py
@@ -16,7 +16,7 @@ class Square:
             ValueError: If size is less than 0.
         """
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("size must be a number")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
