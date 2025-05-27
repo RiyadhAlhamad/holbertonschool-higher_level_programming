a25e916a1eb471ca45210a0c0692643b9f21c11e
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-05-27 10:39:30 +0000

    edit file 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-"""Module that defines a class Lookup"""
+"""Module that defines function lookup with parmeters obj."""
 
 
 def lookup(obj):
+    """Return: The list of available attributes and methods of an object.
+    Args:
+        obj: instance of the class.
+
+    Returns:
+        List of attributes."""
     return dir(obj)
