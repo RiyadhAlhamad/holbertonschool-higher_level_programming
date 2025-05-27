c4b12f1fd5ae9109f0dde1753c61e9684f0a7e32
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-05-27 10:56:44 +0000

    add file 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,12 +6,5 @@ class MyList(list):
     """defines class MyList."""
 
     def print_sorted(self):
-        """def for print and sort list.
-        Args:
-            myList: tack a copy from list class and sorted.
-        Return:
-            return myList sorted.
-        """
-        myList = list.copy(self)
-        myList.sort()
-        return myList
+        """prints the list, but sorted (ascending sort)."""
+        return print(sorted(self))
