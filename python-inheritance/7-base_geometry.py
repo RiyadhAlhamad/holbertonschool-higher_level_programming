b3b36522c53473e240c5e17950ec1b24fa2da82e
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-05-29 10:42:01 +0000

    add file 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module that defines empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Empty class BaseGeometry."""
+
+    def area(self):
+        """fun that raise Exception."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """fun that that validates value."""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
