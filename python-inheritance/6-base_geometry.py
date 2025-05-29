c460ac89e034790ea40a8677e20c36089ec481b9
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-05-29 10:34:01 +0000

    add file 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Empty class BaseGeometry."""
+    
+    def area(self):
+        """fun that raise Exception."""
+        raise Exception("area() is not implemented")
