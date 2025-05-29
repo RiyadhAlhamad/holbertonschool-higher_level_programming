902a1c719a258dbe946ef93105a79133fdd62958
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-05-29 10:08:23 +0000

    edit file 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
-def is_same_class(types, classes):
-    """Return: if types is type of classes or not"""
-    return type(types) is classes
+"""Module that defines function is same class with two attr"""
+
+
+def is_same_class(obj, a_class):
+    """Return: The boolean if obj is instance of a_class or not
+        
+        Args:
+            boolean: to return boolean loge False or True
+    """
+    boolean = False
+    if type(obj) is a_class:
+        boolean = True
+    return boolean
