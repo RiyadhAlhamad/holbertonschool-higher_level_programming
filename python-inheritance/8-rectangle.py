a32d69caa2abcfefc397e54eaadcd92f93bf0652
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-05-29 11:04:05 +0000

    edit file 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""Module that defines empty class Rectangle ingerits BaseGeomety."""
 
 
 class Rectangle(BaseGeometry):
-    """Class Rectangle inherits from BaseGeometry."""
+    """Class that defines a rectangle from BaseGeometry Class."""
 
     def __init__(self, width, height):
         """fun __init__ that instantiation with width and height."""
