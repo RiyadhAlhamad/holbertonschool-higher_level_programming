6cde0eaa32c1376215a3e8fd2b01a20ca1d54c30
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-05-29 10:17:42 +0000

    add file 3 and edit file 2

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Module that defines function is same class."""
+"""Module that defines function is kind of class."""
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """Return: The boolean if obj is instance of a_class or not
 
         Args:
             boolean: to return boolean loge False or True
     """
     boolean = False
-    if type(obj) is a_class:
+    if isinstance(obj, a_class):
         boolean = True
     return boolean
