4719632c065f0b37542222712b5c985a6e2f40b0
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-06-04 15:18:56 +0000

    edit file 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """This function to recive filename to read it to files"""
-    with open as filename:
-        files = open(filename, 'r', encoding="UTF-8")
+    with open(filename, 'r', encoding="UTF-8") as f:
+        files = f.read()
