a953149ffd9b22c8da5708f0baf788d2febba81b
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-06-04 15:11:04 +0000

    add file 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def read_file(filename=""):
+    """This function to recive filename to read it to files"""
+    with open as filename:
+        files = open(filename, 'r', encoding="UTF-8")
