6f573ba34f419146a4671b4fe8c81d7dddfbff03
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-06-04 15:37:04 +0000

    add file 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Moudel read_file to read files"""
+
+
+def write_file(filename="", text=""):
+    """This function to recive filename to read it to files"""
+    with open(filename, 'r') as f:
+        print(f.write(text), end="")
+    return len(text)
