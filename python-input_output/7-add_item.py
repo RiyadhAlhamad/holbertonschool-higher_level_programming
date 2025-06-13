1fe216a4d82ba766d916bf188b6aa84b460e15c4
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-06-13 01:32:50 +0000

    add file 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Moudel to json string"""
-import json
+"""script that adds all arguments to a Python list, and then save them to a file"""
+import sys
 
 
-def save_to_json_file(my_obj, filename):
-    """return save json file"""
-    with open(filename, mode='w', encoding='utf-8') as f:
-        f.write(json.dumps(my_obj))
+save_to_jason_file = __import__('5-save_to_jason_file').save_to_jason_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+file_name = "add_item.json"
+
+item_list = load_from_json_file(file_name)
+item_list.extend(sys.argv[1:])
+save_to_jason_file(item_list, file_name)
