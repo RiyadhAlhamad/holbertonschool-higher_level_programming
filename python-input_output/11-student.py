b9cfec6d91f8fa6303e1bd33472e7ecef4788dcf
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-06-13 02:28:03 +0000

    add file 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+Module that contains class student
+"""
+
+
+class Student:
+    """class student
+
+    Args:
+        first_name: first name of student
+        last_name: last name of studen
+        age: the age of studen
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation method for first, last and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """that retrieves a dictionary representation of a Student instance
+        return:
+            self.__dict
+            or
+            result
+        """
+        if attrs is None:
+            return self.__dict__
+        result = {}
+
+        for attr_name in attrs:
+            if attr_name == "first_name":
+                result["first_name"] = self.first_name
+            elif attr_name == "last_name":
+                result["last_name"] = self.last_name
+            elif attr_name == "age":
+                result["age"] = self.age
+        return result
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance
+        
+        Args:
+            json (dict): dictionary containing attribute names and values
+        """
+        for key, value in json.items():
+            if key == "first_name":
+                self.first_name = value
+            elif key == "last_name":
+                self.last_name = value
+            elif key == "age":
+                self.age = value
