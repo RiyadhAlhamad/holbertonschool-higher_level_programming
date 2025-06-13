283b08ffa528d0992702a9b8882e14bbf97e8ee1
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-06-13 01:46:57 +0000

    edit file 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,11 +3,11 @@
 import sys
 
 
-save_to_jason_file = __import__('5-save_to_json_file').save_to_jason_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 file_name = "add_item.json"
 
 """Try to load existing data from file and save file to json file"""
 item_list = load_from_json_file(file_name)
 item_list.extend(sys.argv[1:])
-save_to_jason_file(item_list, file_name)
+save_to_json_file(item_list, file_name)
