e2b7774114d2339f3dde2b69e21a8e12322c306d
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-06-13 01:51:46 +0000

    edit file 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""script that adds all arguments to a Python list, and then save them to a file"""
+"""
+script that adds all arguments to a Python list,
+and then save them to a file
+"""
 import sys
 
 
