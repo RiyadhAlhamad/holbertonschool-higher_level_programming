7a32c3195ee7500f10e50c9e0b9c340167e9f22e
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-06-19 10:39:29 +0000

    edit file

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -4,10 +4,10 @@ import json
 
 class SimpleAPIHandler(BaseHTTPRequestHandler):
 
-    def do_Get(self):
+    def do_GET(self):
         if self.path == "/":
             self.send_response(200)
-            self.send_headers('Content-type', 'text/plain')
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b"Hello Riyadh Alhamad")
 
@@ -20,20 +20,20 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
 
         elif self.path == "/info":
             self.send_response(200)
-            self.send_headers('Content-type', 'application/json')
+            self.send_header('Content-type', 'application/json')
             self.end_headers()
             info = {"version": "1.0", "description": "A simple API built with http.server"}
             self.wfile.write(json.dumps(info).encode("utf-8"))
 
         elif self.path == "/status":
             self.send_response(200)
-            self.send_headers('Content-type', 'text/plain')
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b"OK")
 
         else:
             self.send_response(404)
-            self.send_headers('Content-type', 'text/plain')
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b"404 Not Found")
 
