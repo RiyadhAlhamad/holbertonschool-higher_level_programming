7737fc02f38bb4d8ef9638f23c36a0ed10bf52fc
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-06-19 10:20:56 +0000

    add file 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -7,33 +7,33 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
     def do_Get(self):
         if self.path == "/":
             self.send_response(200)
-            self.send_headers('content-type', 'text/plain')
+            self.send_headers('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b"Hello Riyadh Alhamad")
 
         elif self.path == "/data":
             self.send_response(200)
-            self.send_header('content-type', 'application/json')
+            self.send_header('Content-type', 'application/json')
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(data).encode('utf-8'))
 
         elif self.path == "/info":
             self.send_response(200)
-            self.send_headers('content-type', 'application/json')
+            self.send_headers('Content-type', 'application/json')
             self.end_headers()
             info = {"version": "1.0", "description": "A simple API built with http.server"}
             self.wfile.write(json.dumps(info).encode("utf-8"))
 
         elif self.path == "/status":
             self.send_response(200)
-            self.send_headers('content-type', 'text/plain')
+            self.send_headers('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b"OK")
 
         else:
             self.send_response(404)
-            self.send_headers('content-type', 'text/plain')
+            self.send_headers('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b"404 Not Found")
 
