59ad3c180c93bcee822bc9be67820825486eb6a4
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-06-29 17:05:49 +0300

    edit file 15

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,9 @@
 -- task 15
 -- comedy only
+
 SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id = (
-    SELECT id FROM tv_genres WHERE name = 'Comedy'
-)
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
