07b3f47c8dcc9908e3b585eee0b7c31f9fb7ef31
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-06-29 17:00:49 +0300

    edit file 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -3,7 +3,4 @@
 SELECT tv_genres.name
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_show_genres.show_id = (
-    SELECT id FROM tv_shows WHERE title = 'Dexter'
-)
-ORDER BY tv_genres.name ASC;
+WHERE tv_show_genres.show_id = (SELECT id FROM tv_shows WHERE title = 'Dexter') ORDER BY tv_genres.name ASC;
